636cb18c71291f64b1ca01339f63f028f7210e11
Alexsik76 <alex@jurist.vn.ua>
2020-04-14 23:35:53 +0300

    3

--- src/tree/render.go
+++ src/tree/render.go
@@ -3,15 +3,22 @@ package main
 import "fmt"
 
 func main() {
-
-	for n := 1; n < 100; n++ {
+	nMax := 55
+	for n := 1; n < nMax; n++ {
+		r := (nMax - n) / 2
+		for i := 1; i < r; i++ {
+			fmt.Print(" ")
+		}
 		for i := 0; i < n; i++ {
 			if i%5 == 0 {
-				fmt.Print("0")
+				fmt.Fprint("", "0")
 			} else {
 				fmt.Print("*")
 			}
 		}
-		fmt.Println(n)
+		for i := 1; i < r; i++ {
+			fmt.Print(" ")
+		}
+		fmt.Println()
 	}
 }
---
+++ src/tree/render2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	nMax := 55
+	for n := 1; n < nMax; n++ {
+		s0 := " "
+		s := "*"
+		d := (nMax - n/2) 
+	fmt.Printf("%-ds", n)
+	}
+}
